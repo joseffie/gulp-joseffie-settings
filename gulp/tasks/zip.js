a7f050a9fac5cc742a0208235bf2c474ed42432d
S4LTER <kros.krosov@inbox.ru>
2022-06-14 20:22:08 +0600

    Now the archive appears in a subdir of the root

--- gulp/tasks/zip.js
+++ gulp/tasks/zip.js
@@ -2,17 +2,17 @@ import del from "del";
 import zipPlugin from "gulp-zip";
 
 export const zip = () => {
-	del(`./${app.path.rootFolder}.zip`);
-	return app.gulp
-		.src(`${app.path.buildFolder}/**/*.*`, {})
-		.pipe(
-			app.plugins.plumber(
-				app.plugins.notify.onError({
-					title: "ZIP",
-					message: "Fix da mistake, leather man: <%= error.message %>",
-				})
-			)
-		)
-		.pipe(zipPlugin(`${app.path.rootFolder}.zip`))
-		.pipe(app.gulp.dest("./"));
+  del(`./${app.path.rootFolder}.zip`);
+  return app.gulp
+    .src(`${app.path.buildFolder}/**/*.*`, {})
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "ZIP",
+          message: "Fix da mistake, leather man: <%= error.message %>",
+        })
+      )
+    )
+    .pipe(zipPlugin(`${app.path.rootFolder}.zip`))
+    .pipe(app.gulp.dest("./project-archive/"));
 };
