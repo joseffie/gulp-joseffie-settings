870cb371a0f55bf1128efecba642d1c731aebe7b
S4LTER <kros.krosov@inbox.ru>
2022-06-21 19:54:50 +0600

    Added pages
    Added pages
    
    Added pages

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,34 +1,35 @@
-import * as nodePath from "path";
+import * as nodePath from 'path';
 const rootFolder = nodePath.basename(nodePath.resolve());
 
 const buildFolder = `./dist`;
 const srcFolder = `./src`;
 
 export const path = {
-	build: {
-		js: `${buildFolder}/js/`,
-		css: `${buildFolder}/css/`,
-		html: `${buildFolder}/`,
-		images: `${buildFolder}/img/`,
-		fonts: `${buildFolder}/fonts/`,
-	},
-	src: {
-		pug: `${srcFolder}/*.pug`,
-		scss: `${srcFolder}/base/scss/main.scss`,
-		js: `${srcFolder}/base/js/index.js`,
-		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
-		svg: `${srcFolder}/img/**/*.svg`,
-		svgicons: `${srcFolder}/svgico/*.svg`,
-	},
-	watch: {
-		js: `${srcFolder}/**/*.js`,
-		scss: `${srcFolder}/**/*.scss`,
-		pug: `${srcFolder}/**/*.pug`,
-		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
-	},
-	clean: buildFolder,
-	buildFolder: buildFolder,
-	srcFolder: srcFolder,
-	rootFolder: rootFolder,
-	// ftp: ``,
+  build: {
+    js: `${buildFolder}/js/`,
+    css: `${buildFolder}/css/`,
+    html: `${buildFolder}/`,
+    images: `${buildFolder}/img/`,
+    fonts: `${buildFolder}/fonts/`,
+  },
+  src: {
+    pug: `${srcFolder}/*.pug`,
+    pages: `${srcFolder}/pages/*.pug`,
+    scss: `${srcFolder}/base/scss/main.scss`,
+    js: `${srcFolder}/base/js/index.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
+    svgicons: `${srcFolder}/svgico/*.svg`,
+  },
+  watch: {
+    js: `${srcFolder}/**/*.js`,
+    scss: `${srcFolder}/**/*.scss`,
+    pug: `${srcFolder}/**/*.pug`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
+  },
+  clean: buildFolder,
+  buildFolder: buildFolder,
+  srcFolder: srcFolder,
+  rootFolder: rootFolder,
+  // ftp: ``,
 };
