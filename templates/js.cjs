41f4fe3042cd0cb9afe760d36fd1715419ffaedb
S4LTER <kros.krosov@inbox.ru>
2023-10-21 22:34:50 +0600

    edit template to autocreate a plugin class

--- templates/js.cjs
+++ templates/js.cjs
@@ -2,4 +2,24 @@
 
 const { EOL } = require('os');
 
-module.exports = () => [].join(EOL);
+module.exports = (entity, naming) => {
+  const fileName = naming.stringify(entity);
+  const className = fileName.charAt(0).toUpperCase() + fileName.slice(1);
+
+  return [
+    'import Plugin from \'@core/Plugin.js\';',
+    'import init from \'@core/init.js\';',
+    '',
+    `class ${className} extends Plugin {`,
+    '  constructor(element, options, name) {',
+    '    super(element, options, name);',
+    '',
+    '    if (!this.isInited()) {',
+    '      this._init();',
+    '    }',
+    '  }',
+    '}',
+    '',
+    `export default init(${className}, '${fileName}');`,
+  ].join(EOL);
+};
